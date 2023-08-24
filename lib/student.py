6fb17ab7bc19b4295967398b651f5cc9f415bd5a
gilbert-ku <gilbert45ku@gmail.com>
2023-08-24 21:49:52 +0300

    70% complete

--- lib/student.py
+++ lib/student.py
@@ -2,7 +2,31 @@
 
 from user import User
 
+# class Student(User):
+#     def __init__(self, first_name, last_name):
+#         super().__init__(first_name, last_name)
+#         self.knowledge = []
+#     def learn(self):
+#         print(self.first_name)
+
+# my_student = Student("My", "Student")
+# print(my_student)
+
+
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
+
+    def learn(self, information):
+        self.knowledge.append(information)
+
+# Creating a Student instance
+my_student = Student("My", "Student")
+
+# Calling the learn() method with an argument
+my_student.learn("New information")
+
+# Printing the student's knowledge
+print(my_student.knowledge)
+
--- lib/teacher.py
+++ lib/teacher.py
@@ -2,9 +2,111 @@
 
 from user import User
 
+# import random
+
+# class User:
+#     def __init__(self, first_name, last_name):
+#         self.first_name = first_name
+#         self.last_name = last_name
+
+# class Teacher(User):
+#     def __init__(self, first_name, last_name, knowledge=None):
+#         super().__init__(first_name, last_name)
+#         self.knowledge = knowledge if knowledge is not None else []
+
+#     def teach(self):
+#         if self.knowledge:
+#             return random.choice(self.knowledge)
+#         else:
+#             return "No knowledge available"
+# knowledge = [
+#     "str is a data type in Python",
+#     "programming is hard, but it's worth it",
+#     "JavaScript async web request",
+#     "Python function call definition",
+#     "object-oriented teacher instance",
+#     "programming computers hacking learning terminal",
+#     "pipenv install pipenv shell",
+#     "pytest -x flag to fail fast",
+# ]
+
+# my_teacher = Teacher("My", "Teacher", knowledge=knowledge)
+# print(my_teacher.first_name)   # Should print "My"
+# print(my_teacher.last_name)    # Should print "Teacher"
+# print(my_teacher.knowledge)    # Should print the list of knowledge
+
+
+# import random
+
+# class User:
+#     def __init__(self, first_name, last_name):
+#         self.first_name = first_name
+#         self.last_name = last_name
+
+# class Teacher(User):
+#     def __init__(self, first_name, last_name, knowledge=None):
+#         super().__init__(first_name, last_name)
+#         self.knowledge = knowledge if knowledge is not None else []
+
+#     def teach(self):
+#         if self.knowledge:
+#             return random.choice(self.knowledge)  # Return a random element from knowledge
+#         else:
+#             return "No knowledge available"
+
+# knowledge = [
+#     "str is a data type in Python",
+#     "programming is hard, but it's worth it",
+#     "JavaScript async web request",
+#     "Python function call definition",
+#     "object-oriented teacher instance",
+#     "programming computers hacking learning terminal",
+#     "pipenv install pipenv shell",
+#     "pytest -x flag to fail fast",
+# ]
+
+# my_teacher = Teacher("My", "Teacher", knowledge=knowledge)
+# print(my_teacher.first_name)   # Should print "My"
+# print(my_teacher.last_name)    # Should print "Teacher"
+# print(my_teacher.knowledge)    # Should print the list of knowledge
+
+# random_knowledge = my_teacher.teach()  # Get a random piece of knowledge
+# print(random_knowledge)
+
 import random
 
+class User:
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
 class Teacher(User):
+    def __init__(self, first_name, last_name, knowledge=None):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge if knowledge is not None else []
 
     def teach(self):
-        pass
+        if self.knowledge:
+            return random.choice(self.knowledge)  # Return a random element from knowledge
+        else:
+            return "No knowledge available"
+
+knowledge = [
+    "str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",
+]
+
+my_teacher = Teacher("My", "Teacher", knowledge=knowledge)
+print(my_teacher.first_name)   # Should print "My"
+print(my_teacher.last_name)    # Should print "Teacher"
+print(my_teacher.knowledge)    # Should print the list of knowledge
+
+random_knowledge = my_teacher.teach()  # Get a random piece of knowledge
+print(random_knowledge)
+
